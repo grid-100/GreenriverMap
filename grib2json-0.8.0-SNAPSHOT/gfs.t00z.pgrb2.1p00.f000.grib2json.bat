efbbc5fa32547000343550f9394485ee6f95de70
grid-100 <ericherlangga@gmail.com>
2018-08-26 07:17:24 +0700

    Add files via upload

---
+++ grib2json-0.8.0-SNAPSHOT/gfs.t00z.pgrb2.1p00.f000.grib2json.bat
@@ -0,0 +1,12 @@
+@ECHO OFF
+REM LIB_DIR=$(dirname "$0")/../lib
+REM LAUNCH_JAR=$LIB_DIR/grib2json-*.jar
+REM $JAVA_HOME/bin/java -Xmx512M -jar $LAUNCH_JAR $@
+SET JAVAPATH=D:/GRM/java/jre-8u92-windows-x64/jre1.8.0_92/bin/java.exe
+SET LIB_DIR=D:/GRM/bin/grib2json-0.8.0-SNAPSHOT/lib
+SET LAUNCH_JAR=%LIB_DIR%/grib2json-0.8.0-SNAPSHOT.jar
+SET TARGETFILENAME=%cd%/gfs.t00z.pgrb2.1p00.f000
+
+REM call %JAVAPATH% -Xmx512M -jar %LAUNCH_JAR% --data --fp 2 --fs 103 --fv 10.0 --output %TARGETFILENAME%.json %TARGETFILENAME%.grib2
+call %JAVAPATH% -Xmx512M -jar %LAUNCH_JAR% --data --fp "wind" --compact --output %TARGETFILENAME%.json %TARGETFILENAME%
+PAUSE
